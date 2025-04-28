7966b9a1deade319113d695c0b3960cc68d5ea5d
Chidike Henry <chidike.henry@gmail.com>
2025-04-28 13:45:37 +0100

    feat:created api-project app for docker testing

---
+++ API_PROJECT/students.js
@@ -0,0 +1,26 @@
+export const movies = [
+    {
+        id: 1,
+        name: "shazam",
+        genre: ["drama", "action"],
+        year: 2017,
+        rating: 13,
+        movieCover: "https://img2.hulu.com/user/v3/artwork/b0f65263-6dee-4843-87c9-98db398731e9?base_image_bucket_name=image_manager&base_image=e9c1b651-aa32-4fa7-af1d-36a96b90c723&size=3200x1800&format=webp"
+    },
+    {
+        id: 2,
+        name: "The batman",
+        genre: ["drama", "action", "fiction"],
+        year: 2022,
+        rating: 10,
+        movieCover: "https://img.hulu.com/user/v3/artwork/54faddb3-3315-4e1f-9f86-3d55409831db?base_image_bucket_name=image_manager&base_image=b6f341e3-c733-406c-9062-f152d563dc61&size=3200x1800&format=webp"
+    },
+    {
+        id: 3,
+        name: "Jack & Jill",
+        genre: ["drama", "thriller"],
+        year: 2024,
+        rating: 15,
+        movieCover: "https://img3.hulu.com/user/v3/artwork/684226fa-76a3-4178-a2b5-929059d1c0ab?base_image_bucket_name=image_manager&base_image=1cf6f7e8-30cf-494b-b7d0-f4b3f61cb1c7&size=3200x1800&format=webp"
+    },
+]
